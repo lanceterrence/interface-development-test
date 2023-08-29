b94dbf5921f56fe41d1817caaa110d46272ed935
Lance Terrence <magsombol.lance@gmail.com>
2023-08-30 00:00:27 +0800

    chore: first block initial ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,50 @@
-import Image from 'next/image'
+import clsx from 'clsx'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <>
+            <main className={clsx([
+                'w-[min(1200px,100%-2rem)] mx-auto',
+                '[&>section]:min-h-screen',
+                'flex flex-col'
+            ])}>
+                {/* First block */}
+                <section className={clsx([
+                    'grid grid-cols-3 grid-rows-2 gap-5',
+                    'py-40'
+                ])}>
+                    <img src={'/component_one/Image-01.jpg'} alt='man walking with a kid'
+                         className='col-start-1 col-end-2 row-start-1 row-end-3'/>
+                    <img src={'/component_one/Image-02.jpg'} alt='fruits'
+                         className='col-start-2 col-end-3 row-start-1 row-end-2'/>
+                    <img src={'/component_one/Image-03.jpg'} alt='asparagus'
+                         className='col-start-2 col-end-3 row-start-2 row-end-3'/>
+                    <div className={clsx([
+                        'col-start-3 col-end-4 row-start-1 row-end-3',
+                        'flex flex-col gap-3 py-2'
+                    ])}>
+                        <h5 className='uppercase text-[28px] font-light'>Answer your body&apos;s needs</h5>
+                        <hr/>
+                        <p className='text-brand-secondary font-light text-lg mt-2 leading-[1.7]'>The way ingredients
+                            are sourced affects the way we nourish our bodies. Author Mark Schatzer believes our body
+                            naturally develops an appetite for the foods and nutrients it needs to be healthy, but that
+                            artificial flavourings are getting in the way. This can be reversed by focusing on
+                            high-quality ingredients and being mindful as your appetite guides you to consume according
+                            to your body&apos;s needs.</p>
+                        <h6 className='text-brand-primary uppercase text-md font-medium tracking-wide mt-5'>Be
+                            mindful</h6>
+                        <p className='text-lg font-medium leading-[1.7]'>Sourcing local or organic food is a good way to
+                            start being more mindful about what you're cooking and eating.</p>
+                    </div>
+                    {/* Second block */}
+                </section>
+                <section className='flex flex-col gap-14 justify-center items-center'>
+                    {/*<h1 className='text-4xl font-light uppercase tracking-wide'>All the latest from AEG</h1>*/}
+                    {/*<div className='w-full h-full min-h-[450px] border'>*/}
+                    {/*    test*/}
+                    {/*</div>*/}
+                </section>
+            </main>
+        </>
+    )
 }
